d3acd2db7168658472ee3d6a1d079f9e5d44defa
Douglas Ward <douglas.ward@prolly.org>
2020-04-14 14:23:35 -0400

    initial commit

---
+++ js/main.js
@@ -0,0 +1,40 @@
+var backEventListener = null;
+
+var unregister = function() {
+    if ( backEventListener !== null ) {
+        document.removeEventListener( 'tizenhwkey', backEventListener );
+        backEventListener = null;
+        window.tizen.application.getCurrentApplication().exit();
+    }
+};
+
+//Initialize function
+var init = function () {
+    // register once
+    if ( backEventListener !== null ) {
+        return;
+    }
+    
+    // TODO:: Do your initialization job
+    $("#contentFrame").height = window.height;
+    console.log("init() called");
+    
+    var backEvent = function(e) {
+        if ( e.keyName === "back" ) {
+     //       try {
+                // move previous page
+            	$("#contentFrame").window.history.back();
+     //       } catch( ex ) {
+       //         unregister();
+         //   }
+        }
+    };
+    
+    // add eventListener for tizenhwkey (Back Button)
+    document.addEventListener( 'tizenhwkey', backEvent );
+    backEventListener = backEvent;
+//	document.addEventListener( 'keydown', setFocusElement );
+};
+
+$(document).bind( 'pageinit', init );
+$(document).unload( unregister );
